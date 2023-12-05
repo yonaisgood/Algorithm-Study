98cb8d02aaf997ecf9965c9c3193ae9828d4d535
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-06 00:53:32 +0900

    [level unrated] Title: 수 조작하기 1, Time: 3.21 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -0,0 +1,16 @@
+function solution(n, control) {
+    var answer = n;
+    
+    for(let i=0; i<control.length; i++){
+        if(control[i] == "w"){
+            answer += 1
+        }else if(control[i] == "s"){
+            answer -= 1
+        }else if(control[i] == "d"){
+            answer += 10
+        }else{
+            answer -= 10
+        }
+    }
+    return answer;
+}
