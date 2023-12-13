5eabae3b5bbc403917d25bda9c47a48de900e523
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-14 01:58:59 +0900

    [level unrated] Title: 5명씩, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -3,8 +3,8 @@ function solution(names) {
     
     
     for(let i = 0; i<names.length; i+=5){
-        answer.push(names.slice(i,i+5))
+        answer.push(names[i])
     }
-    let answerGroup = answer.map(subArr => subArr[0]);
-    return answerGroup;
+    
+    return answer;
 }
