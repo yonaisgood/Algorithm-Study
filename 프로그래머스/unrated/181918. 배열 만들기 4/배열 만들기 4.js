81706d9f262c1f518589b04e7e619a4ae0013dae
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-14 15:05:57 +0900

    [level unrated] Title: 배열 만들기 4, Time: 4.97 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -1,15 +1,4 @@
 function solution(arr) {
-    let stk = [];
-    
-    
-    for(let i = 0; i < arr.length; i++){
-    let last = stk[stk.length-1];
-        if(stk.length == 0 || last < arr[i]){
-            stk.push(arr[i])  
-        }else  if (last >= arr[i]){
-            stk.pop()
-            i--
-        }
-    }
+    var stk = [];
     return stk;
 }
