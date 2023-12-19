344fc53225117aff6d78bc8fff372dffb589f46e
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-20 04:02:41 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.js
@@ -0,0 +1,12 @@
+function solution(arr, idx) {
+    let answer = 0;
+    
+    for(let i = idx; i < arr.length; i++){
+        let cur = arr[i];
+        if(cur == 1)
+            return i
+        }
+    return -1;       
+    }
+    
+
