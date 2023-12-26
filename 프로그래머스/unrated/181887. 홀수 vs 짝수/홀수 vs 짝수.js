d914fc49a85e2e0cd532c7368d849110b826da66
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-27 01:27:03 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -1,15 +1,9 @@
 function solution(num_list) {
-    
     let odd = 0;
     let even = 0;
-    
-    for (let i = 0; i < num_list.length; i++){
-        if(i % 2 == 0){
-            odd += num_list[i];
-        }
-        else{
-            even += num_list[i];
-        }
-    }
+   
+    num_list.map((v,idx)=>
+                (idx % 2 == 0)? odd += v : even += v)
+   
     return odd >= even? odd : even;
 }
