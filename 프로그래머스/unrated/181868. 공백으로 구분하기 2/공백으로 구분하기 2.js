a440807014bfa22f9505fba879800802d7da2870
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-28 22:13:05 +0900

    [level unrated] Title: 공백으로 구분하기 2, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,10 +1,5 @@
 function solution(my_string) {
-    let answer = [];
-    let stringSplit = my_string.split(" ");
-    for(let i = 0; i < stringSplit.length; i++){
-        if(stringSplit[i] != ""){
-            answer.push(stringSplit[i])
-        }
-    }
-    return answer;
+    let stringSplit = my_string.split(" ").filter(v=>v);
+    
+    return stringSplit;
 }
