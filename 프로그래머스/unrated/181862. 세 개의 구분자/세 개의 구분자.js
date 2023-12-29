c726bb8566f25998d59f3b50a613626a5f731ed4
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2023-12-30 01:41:04 +0900

    [level unrated] Title: 세 개의 구분자, Time: 16.40 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,7 +1,4 @@
-function solution(myStr) {   
-
-    let sliceString = myStr
-    .split("a").join("_").split("b").join("_").split("c").join("_").split("_").filter(v=>v.length != 0)
-    
-    return sliceString.length > 0 ? sliceString : ["EMPTY"] ;
+function solution(myStr) {
+    const list = myStr.split(/[abc]/).filter(str => str);
+    return list.length ? list : ["EMPTY"];
 }
