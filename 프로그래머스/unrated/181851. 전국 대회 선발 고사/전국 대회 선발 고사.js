8c42c836c0f8c9d37921c5df68ccde396b3efc83
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-05 01:59:59 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.js
@@ -1,16 +1,7 @@
 function solution(rank, attendance) {
-    let result = [];
-    
-    for (let i = 0; i < rank.length; i++){
-        if(attendance[i]){
-            result.push(rank[i])
-        }
-    }
-    result.sort((a,b)=>a-b);
-    
-    function answer(idx) {
-        return rank.indexOf(result[idx]);
-    }
-      
-    return (10000 * answer(0)) + (100 * answer(1)) + answer(2)
+  const [a, b, c] = rank
+    .map((el, idx) => [el, idx])
+    .filter(([_, i]) => attendance[i])
+    .sort(([a], [b]) => a - b);
+  return 10000 * a[1] + 100 * b[1] + c[1];
 }
