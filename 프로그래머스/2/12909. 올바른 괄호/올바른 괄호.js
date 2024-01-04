504e51bde752102dab212d398e2734d2aa6718cf
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-04 17:26:31 +0900

    [level 2] Title: 올바른 괄호, Time: 3.14 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,11 +1,15 @@
-function solution(s){    
-    const stack = [];    
-    for(let i = 0; i < s.length; i++) {
-        if (stack[stack.length - 1] === '(' && s[i] === ')') {
-            stack.pop();
-        } else {
-            stack.push(s[i]);
+function solution(s){
+    let answer = 0;
+    if (s[0] == ')') return false
+    
+    for(let i = 0; i < s.length; i++){        
+        if(s[i] == '('){
+            answer += 1
+        }
+        else if(s[i] == ')'){
+            answer -= 1 
+            if(answer < 0) return false
         }
     }    
-    return !stack.length;
+    return !answer 
 }
