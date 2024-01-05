43ac0ba568f8111aac7274124eb9366fbc1063e9
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-05 21:11:27 +0900

    [level 2] Title: 의상, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,18 @@
+function solution(clothes) {
+    const clothesObj = {}
+    clothes.forEach(([cloth,type])=>{
+        if(clothesObj[type] === undefined){
+            clothesObj[type] = 1
+        }
+        else{
+            clothesObj[type]++
+        }
+    })
+
+    const clothesCount = Object.entries(clothesObj).reduce((acc,[_,clothesTypeCount])=>{
+        return acc*(clothesTypeCount+1)
+    },1)
+
+    const answer = clothesCount - 1
+    return answer    
+}
