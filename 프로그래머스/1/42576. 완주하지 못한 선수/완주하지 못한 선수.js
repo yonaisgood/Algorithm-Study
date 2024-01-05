793fe8757b7b5a2c2dea120ddf2f72bde87ba0ba
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-06 01:43:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.56 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -2,7 +2,7 @@ function solution(participant, completion) {
     const result = {};
     
     for (const name of participant){
-        result[name] = (result[name] || 0) + 1;
+        result[name] = result[name] ? result[name]+1 : 1;
     }
     
     for (const name of completion){
