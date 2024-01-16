6ac06c2e69469a94dc3a0b5c74fd9eaffe858bcc
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-16 18:50:45 +0900

    [level Lv.1] Title: 행렬의 덧셈, Time: 8.96 ms, Memory: 67.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+   let answer = [];
+    
+    for (let i = 0; i < arr1.length; i++){
+        const tempArr = [];
+        
+        for (let j = 0; j < arr1[i].length; j++){
+            tempArr.push(arr1[i][j] + arr2[i][j])
+        }
+        answer.push(tempArr)
+    }
+    
+   return answer;
+}
