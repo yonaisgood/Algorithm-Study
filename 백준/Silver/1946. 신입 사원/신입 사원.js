50baa99129ded4a21a382e9a7161e2be3e74b95f
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-18 21:17:35 +0900

    [Silver I] Title: 신입 사원, Time: 4480 ms, Memory: 399272 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+input.shift()
+let testGrade = [];
+
+while (input.length > 0){
+    const peopleCount = Number(input.shift());
+    const paperReport = input.splice(0, peopleCount).map((v)=>v.split(" ").map(Number)).sort((a,b)=>a[0] - b[0])
+
+    testGrade.push(paperReport) 
+}
+
+
+testGrade.forEach((item)=>{
+    let passCount = 1;
+    let interviewReport = item[0][1]; //4
+
+    for(let i = 1; i < item.length; i++){
+        let nowInterviewReport = item[i][1];
+        if(interviewReport > nowInterviewReport){
+            passCount += 1;
+            interviewReport = nowInterviewReport
+        }
+    }
+ console.log(passCount)
+})
