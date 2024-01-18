9f79bcb2ce7bbbbadf8a17be1b9f6e684bb6f65e
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-18 19:46:30 +0900

    [Silver III] Title: 주유소, Time: 216 ms, Memory: 19668 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const input = fs.readFileSync('/dev/stdin').toString().trim().split("\n");
+
+const cityCount = input.shift();
+const distanceCount = input.shift().split(" ").map(Number);
+const oilCost = input.shift().split(" ").map(Number);
+
+let distanceCost = 0;
+let currentPrice = oilCost[0];
+for (let i = 0; i < cityCount -1; i++){
+    distanceCost += currentPrice * distanceCount[i]
+
+    if(currentPrice > oilCost[i + 1]){
+        currentPrice = oilCost[i + 1]
+    }
+}
+
+
+console.log(distanceCost)
