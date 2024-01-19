3aa296e9170cf90de9f2352cc2e3c9efd1467c28
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-19 19:55:35 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -1,21 +1,19 @@
 function solution(arr) {
-    let idx = 0
-    let prevArr = arr
-    while(true) {
-        
+    let idx = 0;
+    let prevArr = arr;
+    
+    while(true) {        
         const changeCurArr = prevArr.map(a => {
-            if(a >= 50 && a%2 === 0) return a/2
-            if(a < 50 && a%2 === 1) return a*2+1
-            return a
-        })
+            if(a >= 50 && a % 2 === 0) return a / 2
+            if(a < 50 && a % 2 === 1) return a * 2+1
+            return a    
+        })        
+        const AllSame = prevArr.every((a, i) => a === changeCurArr[i])
         
-        const isAllSame = prevArr.every((a, i) => a === changeCurArr[i])
-        if(isAllSame) break
-        idx+=1
-        
-        
+        if(AllSame) break        
+        idx += 1      
         prevArr = changeCurArr
     }
     
-    return idx
+    return idx;
 }
