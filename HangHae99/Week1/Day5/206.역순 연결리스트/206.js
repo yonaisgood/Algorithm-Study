cecf6eea33dc1791c7ea9325feefa98632f5a642
yonainthefish <yonaisgood@gmail.com>
2024-01-22 02:18:37 +0900

    Week1/Day5(연결리스트)

---
+++ HangHae99/Week1/Day5/206.역순 연결리스트/206.js
@@ -0,0 +1,13 @@
+var reverseList = function(head) {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        let nextNode = current.next;
+        current.next = prev;
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev;
+};
---
+++ HangHae99/Week1/Day5/328.홀짝 연결 리스트/328.js
@@ -0,0 +1,29 @@
+var oddEvenList = function(head) {
+    if(head == null || head.next == null || head.next.next == null){
+        return head;
+    }
+    let odd = new ListNode(head.val);
+    let even = new ListNode(head.next.val);
+    let oddPtr = odd;
+    let evenPtr = even;
+    let ptr = head.next.next;
+    let count = 2;
+    while(ptr != null){
+        count++;
+        if(count % 2 == 0){
+            let n = new ListNode(ptr.val);
+            evenPtr.next = n;
+            evenPtr = evenPtr.next;
+        }
+        else{
+            let n = new ListNode(ptr.val);
+            oddPtr.next = n;
+            oddPtr = oddPtr.next;
+        }
+        if(ptr.next == null){
+            oddPtr.next = even;
+        }
+        ptr = ptr.next;
+    }
+    return odd;
+};
