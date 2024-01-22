ae572a2046b28237b3d8905b94d783d972e14a5e
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-01-22 12:04:54 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,15 +1,9 @@
 function solution(t, p) {
-    const NumberP = parseInt(p)
     let count = 0;
     
     for(let i = 0; i <= t.length - p.length; i++){        
-        const substringT = t.substring(i, i + p.length);
-        const NumberT = parseInt(substringT)
-        
-        if (NumberT <= NumberP) {
-        count++;
-        }
-    }  
-    
+        const substringT = t.substring(i, i + p.length);        
+        if (+substringT <= +p) count++;        
+    }      
     return count;
 }
