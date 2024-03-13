1d23195c9582e46bb78c6840090aec886c5b3e4b
HaEun Ryu <124084624+yonainthefish@users.noreply.github.com>
2024-03-14 03:56:14 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,4 +1,4 @@
 function solution(strings, n) {
-    var sortedStr = strings.sort();
+    let sortedStr = strings.sort();
     return sortedStr.sort((a, b) => a[n].charCodeAt() - b[n].charCodeAt());
 }
