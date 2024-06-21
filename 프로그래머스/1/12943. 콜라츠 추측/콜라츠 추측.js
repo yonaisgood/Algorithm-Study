8e6143922379a50bda781e5d6a2ddb91f62fdccf
yona☁️ <124084624+yonaisgood@users.noreply.github.com>
2024-06-21 14:18:11 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,7 +2,7 @@ function solution(num) {
     let answer = 0;
     
     function collatz(calNum){
-        if(answer > 500) return -1;
+        // if(answer > 500) return -1;
         if(calNum === 1) return answer;
         answer += 1
         calNum % 2 == 0? collatz(calNum / 2) : collatz((calNum * 3 ) + 1)
