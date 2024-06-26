d9ee67da29ad379baf164d2716aef9743f0bfe9c
yona☁️ <124084624+yonaisgood@users.noreply.github.com>
2024-06-26 14:31:04 +0900

    [level 1] Title: 시저 암호, Time: 1.17 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,17 +1,4 @@
 function solution(s, n) {
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer = '';
-
-    for(var i = 0; i < s.length; i++) {
-        let text = s[i];
-        if(text == ' ') {
-            answer += ' ';
-        } else {
-            let textArr = upper.includes(text) ? upper : lower;
-            let index = (textArr.indexOf(text) + n) % textArr.length;
-            answer += textArr[index];
-        }
-    }
-    return answer;
+    var chars = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY                          "
+    return s.split('').map(e => chars[chars.indexOf(e)+n]).join('');
 }
