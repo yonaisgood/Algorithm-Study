02c19f674c10912e93c75bd1400f267b24eda6cb
yona☁️ <124084624+yonaisgood@users.noreply.github.com>
2024-06-26 14:23:58 +0900

    [level 1] Title: 시저 암호, Time: 1.35 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/README.md
+++ 프로그래머스/1/12926. 시저 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 시저 암호 - 12926 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12926) 
 
 ### 성능 요약
 
-메모리: 37.2 MB, 시간: 5.74 ms
+메모리: 33.8 MB, 시간: 1.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 21일 22:02:31
+2024년 06월 26일 14:23:53
 
 ### 문제 설명
 
--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,18 +1,17 @@
 function solution(s, n) {
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var lower = "abcdefghijklmnopqrstuvwxyz";
-    var answer= '';
+    var answer = '';
 
-    for(var i =0; i <s.length; i++){
-        var text = s[i];
+    for(var i = 0; i < s.length; i++) {
+        let text = s[i];
         if(text == ' ') {
-            answer += ' '; 
-            continue;
+            answer += ' ';
+        } else {
+            let textArr = upper.includes(text) ? upper : lower;
+            let index = (textArr.indexOf(text) + n) % textArr.length;
+            answer += textArr[index];
         }
-        var textArr = upper.includes(text) ? upper : lower;
-        var index = textArr.indexOf(text)+n;
-        if(index >= textArr.length) index -= textArr.length;
-        answer += textArr[index];
     }
     return answer;
 }
