c71390893c5c02aaf28827b619273b1778fd1e45
yona☁️ <124084624+yonaisgood@users.noreply.github.com>
2024-06-27 13:28:13 +0900

    [level 0] Title: 가위 바위 보, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,9 +1,7 @@
 function solution(rsp) {
     var answer = '';
-    let splitWord = rsp.split("")
     
-    for (let i=0; i < splitWord.length; i++){
-        let cha = splitWord[i]
+    for (let cha of rsp){
         if(cha === "2") answer += 0
         if(cha === "0") answer += 5
         if(cha === "5") answer += 2
