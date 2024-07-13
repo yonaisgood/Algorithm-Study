5eb4071bd29fcd20c5d4c5fde41450b93f0a7351
yona☁️ <124084624+yonaisgood@users.noreply.github.com>
2024-07-13 14:59:08 +0900

    [level 2] Title: 전화번호 목록, Time: 148.98 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,16 +1,13 @@
 function solution(phone_book) {
-    let phoneNumber = {};
-    
-    for(const number of phone_book){
-        phoneNumber[number] = true
-    }
-    
-    for(const number of phone_book){
-        for(let i = 1; i < number.length; i++){
-            const checkNum = number.slice(0,i)    
-            if(phoneNumber[checkNum])
-            return false
+    // 전화번호부를 사전 순서대로 정렬
+    phone_book.sort();
+
+    // 정렬된 전화번호부에서 인접한 번호들만 비교
+    for (let i = 0; i < phone_book.length - 1; i++) {
+        if (phone_book[i + 1].startsWith(phone_book[i])) {
+            return false;
         }
     }
+
     return true;
 }
