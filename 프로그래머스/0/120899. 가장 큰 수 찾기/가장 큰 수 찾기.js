8ece24cf89aafb5737e8e9f594883384f2a25254
Yona Park☁️ <124084624+yonaisgood@users.noreply.github.com>
2025-01-02 23:52:13 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,12 +1,4 @@
 function solution(array) {
-    let answer = [];
-    let nowNum = 0;
-    
-    for (let i = 0; i < array.length; i++){
-        if(array[i] >= nowNum) {
-            nowNum = array[i]
-            answer=[array[i],i]
-        }
-    }
-    return answer;
+    let answer = Math.max(...array)
+    return [answer, array.indexOf(answer)];
 }
