fb4bbe3a7c42c5fadc6892aa0c1729b05b1b32cc
Yona Park☁️ <124084624+yonaisgood@users.noreply.github.com>
2025-01-04 21:44:05 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -1,9 +1,3 @@
 function solution(n, numlist) {
-    var answer = [];
-    for (let i = 0; i < numlist.length; i++){
-        if (numlist[i] % n === 0 ) {
-            answer.push(numlist[i])
-        }
-    }
-    return answer;
+    return numlist.filter(v => v % n === 0)
 }
